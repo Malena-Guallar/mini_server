7bf2bcf8a7c2d3e3b2bb2c6b19594b205931bbed
Malena Guallar <malena.guallar@gmail.com>
2023-06-22 17:39:20 +0200

    able to make post requests with new data

--- json.js
+++ json.js
@@ -3,34 +3,28 @@ const http = require("http");
 const host = 'localhost';
 const port = 8000;
 
-console.log('welcome to my server')
-
-
-
-
+const prompt = require('prompt-sync')({sigint: true});
 
 const handleRequest = (req, res) => {
 
-const prompt = require('prompt-sync')({sigint: true});
-const choice = prompt('Would u like to add or read a quote? : ');
-console.log(`~ you chose : ${choice}`);
+    const choice = prompt('Would u like to add or read a quote? : ');
+    console.log(`~ you chose : ${choice}`);
 
-    if (choice === "read"){
-        getRequest(req, res)
-    } else if (choice === "add"){
-        postRequest(req, res)
-    } else {
-        console.log("invalid choice");
-        console.log(" _ _ _ _ _ _ _ _")
+        if (choice === "read"){
+            getRequest(req, res)
+        } else if (choice === "add"){
+            postRequest(req, res)
+        } else {
+            console.log("invalid choice");
+            console.log(" _ _ _ _ _ _ _ _")
 
-    }
-
+        }
 }
 
 
-const getRequest = function (req, response) {
-        
-    fetch("https://www.affirmations.dev/")
+const getRequest = async (req, response) => {
+
+    await fetch("https://www.affirmations.dev/")
       .then((apiResponse) => apiResponse.json())
       .then((json) => {
             console.log(json);
@@ -42,46 +36,31 @@ const getRequest = function (req, response) {
 };
 
 
-// The res.setHeader() method adds an HTTP header to the response. 
-// HTTP headers are additional information that can be attached to a request or a response.
-// The Content-Type header is used to indicate the format of the data, also known as media type, 
-// that’s being sent with the request or response. In this case our Content-Type is application/json.
+const postRequest = async (req, res) => {
+
+    const quote = prompt("Type here your quote : ");
+    console.log(quote);
+
+    await fetch("http://localhost:8000", {
+        method: "POST",
+        body: JSON.stringify(quote)
+    })
+        .then((apiResponse) => apiResponse.json())
+        .then((json) => {
+            console.log(json);
+            let data = JSON.stringify(json);
+            response.setHeader("Content-Type", "application/json")
+            response.end(data);
+        })
 
 
-let quotes = {
-    affirmation: "T'es la meilleure bb"
-    }
-
-
-const postRequest = (req, res) => {
-
-    let body = "";
-    req.on("data", (chunk) => {
-        body += chunk;
-    });
-    
-    req.on("end", () => {
-        try {
-            const postData = JSON.parse(body);
-            console.log("POST request:", postData);
-            const response = { message: "POST request received" };
-            let data = JSON.stringify(response);
-            res.setHeader("Content-Type", "application/json");
-            res.end(data);
-
-        } catch (error) {
-            console.error(error);
-            res.statusCode = 400;
-            res.end();
-        }
-    });
 };
 
       
 
 const server = http.createServer(handleRequest);
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+    console.log(`Welcome to my server`);
 });
 
 
