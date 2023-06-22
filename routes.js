d9b98ac2b3188a5e2aeea76181e2cdbbb7e116af
Malena Guallar <malena.guallar@gmail.com>
2023-06-22 10:52:54 +0200

    managing routes so we can access various json resources

--- routes.js
+++ routes.js
@@ -1,9 +1,42 @@
+// data variables in json 
+const books = JSON.stringify([
+    { title: "The Alchemist", author: "Paulo Coelho", year: 1988 },
+    { title: "The Prophet", author: "Kahlil Gibran", year: 1923 }
+]);
+
+const authors = JSON.stringify([
+    { name: "Paulo Coelho", countryOfBirth: "Brazil", yearOfBirth: 1947 },
+    { name: "Kahlil Gibran", countryOfBirth: "Lebanon", yearOfBirth: 1883 }
+]);
+
+
+
 const http = require("http");
 
 const host = 'localhost';
 const port = 8000;
 
-const requestListener = function (req, res) {};
+
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    switch (req.url) {
+        case "/books":
+            res.writeHead(200);
+            res.end(books);
+            break
+        case "/authors":
+            res.writeHead(200);
+            res.end(authors);
+            break
+        default:
+            res.writeHead(404);
+            res.end(JSON.stringify({error:"Resource not found"}));
+    }
+    // the switch case function will takes a particular endpoint and returns the corresponding data
+    // we return an error if the user tries to go to another path
+    
+
+};
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
